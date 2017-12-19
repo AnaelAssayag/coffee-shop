1ff4c78797370629c1b72a216a496a6be7698eff
Anael Dahan <anael.assayag@gmail.com>
2017-12-19 13:09:10 +0200

    add title

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var coffeeShop = {
   beans: 40,
+  money: 100,
+  beansCost: 5,
 
   drinkRequirements: {
     latte: 10,
@@ -7,12 +9,33 @@ var coffeeShop = {
     doubleShot: 15,
     frenchPress: 12
   },
-
+  
   makeDrink: function (drinkType) {
-    // TODO: Finish this method
+
+    if(! this.drinkRequirements[drinkType]) {
+      alert('Sorry, we don\'t make ' + drinkType)
+    }
+    else if (this.beans >= this.drinkRequirements[drinkType]){
+      this.beans = this.beans - this.drinkRequirements[drinkType]
+    }
+    else {
+      alert('Sorry, we\'re all out of beans')
+    }
+  },
+  
+  buySupplies: function(beanPurchased) {
+    if((this.beansCost * beanPurchased) > this.money) {
+      alert('Not enough money')
+    }else{
+    this.money -= (beanPurchased * this.beansCost)
+    this.beans += beanPurchased
+    }
+
   }
 }
 
+
+coffeeShop.buySupplies(15)
 coffeeShop.makeDrink("latte");
 coffeeShop.makeDrink("americano");
 coffeeShop.makeDrink("filtered");
